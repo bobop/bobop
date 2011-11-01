6d0180e74db9387788a28a944c65ea9f3d60dbbc
Ben Gibbs <ben@bobop.co.uk>
2011-11-01 20:39:54 +0000

    added pages controller and a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Bobop::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+  
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
