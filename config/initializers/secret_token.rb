14901b3803e1b7c22722b40f28fef13bd1c04acd
Ben Gibbs <ben@bobop.co.uk>
2011-11-01 20:27:30 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bobop::Application.config.secret_token = '767ba57c6dbd4b70ecd438611a1f0add99fe2cc59de959dfd07d6f587e4a3c0b08da40262fb16e12980f29d02800159f02575debdb39daade88c009f1c90462d'
