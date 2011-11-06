2f7143e1eb0e3c9f532dd9737b444c9a6d83590b
Ben Gibbs <ben@bobop.co.uk>
2011-11-06 20:19:43 +0000

    allowed projects to have no client

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :client
-  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x50>" },
        :storage => :s3,
        :s3_credentials => "#{Rails.root}/config/s3.yml",
        :path => "/:style/:id/:filename"
