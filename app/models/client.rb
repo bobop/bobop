c6b01ef05aba15dee34737dd5bfe139943389474
Ben Gibbs <ben@bobop.co.uk>
2011-11-06 21:23:24 +0000

    changed client logo thumb size

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base
   has_many :projects
-  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x50>" },
        :storage => :s3,
        :s3_credentials => "#{Rails.root}/config/s3.yml",
        :path => "/:style/:id/:filename"
