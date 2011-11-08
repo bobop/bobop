431a5a807bb14df0d3644554a4b05e413e2226b0
Ben Gibbs <ben@bobop.co.uk>
2011-11-08 15:43:41 +0000

    layout changes to client and projects list pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def home
     @clients = Client.limit(5).all
-    @projects = Project.limit(5).all
+    @projects = Project.limit(5).order("completed_at DESC").all
   end
 
   def services
