b5ba6cdb9ad0fbde04e96d0587247ef0420a6664
Ben Gibbs <ben@bobop.co.uk>
2011-11-21 16:05:46 +0000

    swapped clients with posts on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @clients = Client.limit(4).all
     @projects = Project.limit(4).order("completed_at DESC").all
+    @posts = Post.limit(4).order("created_at DESC").all
   end
 
   def services
